eac40027108eea7545da5e2c1f95c1724075b215
Malisha A.P.D <128911037+Malisha4065@users.noreply.github.com>
2025-05-27 23:50:03 +0530

    bat file for windows

---
+++ initial.bat
@@ -0,0 +1,20 @@
+@echo off
+REM Stop and remove containers
+docker compose down
+
+REM Build the hadoop-client image
+docker compose build hadoop-client
+
+REM Change ownership of the namenode data directory
+docker compose run --rm --user root namenode chown -R 1000:1000 /opt/hadoop/dfs/name
+
+REM Change ownership of the datanode data directory
+docker compose run --rm --user root datanode chown -R 1000:1000 /opt/hadoop/dfs/data
+
+REM Start the containers
+docker compose up -d
+
+REM Execute the run-jobs.sh script inside the hadoop-client container
+docker compose exec hadoop-client /opt/hadoop/scripts/run-jobs.sh
+
+echo "Initial script finished. Check Docker Desktop for container status."
