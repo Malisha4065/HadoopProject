759897a6e99507b0b6e4623383247cda74f539cd
Malisha A.P.D <128911037+Malisha4065@users.noreply.github.com>
2025-06-05 16:30:31 +0530

    add another datanode

--- initial.sh
+++ initial.sh
@@ -6,11 +6,19 @@ docker compose down
 # Build the hadoop-client image
 docker compose build hadoop-client
 
+# Remove the NameNode directory
+docker compose run --rm --user root namenode rm -rf /opt/hadoop/dfs/name/*
+
 # Change ownership of the namenode data directory
 docker compose run --rm --user root namenode chown -R 1000:1000 /opt/hadoop/dfs/name
 
-# Change ownership of the datanode data directory
+docker compose run --rm namenode hdfs namenode -format -force
+
+docker compose run --rm --user root datanode rm -rf /opt/hadoop/dfs/data/*
 docker compose run --rm --user root datanode chown -R 1000:1000 /opt/hadoop/dfs/data
+
+docker compose run --rm --user root datanode2 rm -rf /opt/hadoop/dfs/data/*
+docker compose run --rm --user root datanode2 chown -R 1000:1000 /opt/hadoop/dfs/data
 
 # Start the containers in detached mode
 docker compose up -d
